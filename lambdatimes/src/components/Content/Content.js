5474d781081e83baf14c38eb39d8d0e622c6c794
Mariana <mhuh0176@gmail.com>
2019-04-05 18:38:39 -0700

    added to content,tabs and tab components

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -17,11 +17,20 @@ export default class Content extends Component {
   }
 
   componentDidMount() {
+    const tabs = tabData;
+    const cards = cardData;
     // Once the component has mounted, get the data and reflect that data on the state.
+    setTimeout(() => {
+      this.setState({
+        tabs,
+        cards,
+      })
+    }, 800);
   }
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
@@ -37,7 +46,9 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === 'all') return this.state.cards;
+    const cards = [...this.state.cards];
+    return cards.filter(card => card.tab === this.state.selected);
   };
 
   render() {
@@ -48,7 +59,10 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs tabs={this.state.tabs}
+          selectedTab={this.state.selected}
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
